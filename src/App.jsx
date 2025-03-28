fa93e7dee3213867ebaa8c1c7199bdd450ccce2f
Razieh <raziye_f@yahoo.com>
2025-03-28 15:17:49 +0100

    post detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { PostProvider } from "./context/PostContext";
-import MainTemplate from "./layout/MainTemplate";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 import PostDetail from "./pages/PostDetail";
+import UpdatePost from "./pages/UpdatePost";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
       <PostProvider>
-        <Routes>
-          <Route path="/" element={<MainTemplate />}>
-            <Route index element={<Home />} />
-            <Route path="createPost" element={<CreatePost />} />
-            <Route path="/posts/:id" element={<PostDetail />} />
-          </Route>
-        </Routes>
+          <Navbar />
+          <main className="px-11 overflow-auto h-[90vh] pt-6">
+            <Routes>
+                <Route index element={<Home />} />
+                <Route path="createPost" element={<CreatePost />} />
+                <Route path="/posts/:id" element={<PostDetail />} />
+                <Route path="/updatePost/:id" element={<UpdatePost />} />
+            </Routes>
+          </main>
       </PostProvider>
     </Router>
   );
